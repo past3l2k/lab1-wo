f4c75122993b45560d0f70db278681edc7d29d59
student <jan.kowalski@pollub.edu.pl>
2023-10-05 15:57:07 +0200

    cos3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,6 +11,7 @@ public class Main {
         System.out.println("cosss");
         System.out.println("123123");
         System.out.println("123112332311323");
+        System.out.println("1231fdgdfgdg23");
     }
 
 }

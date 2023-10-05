83553d057f4cdb1b4a18554d7553a4c357a2a7db
student <jan.kowalski@pollub.edu.pl>
2023-10-05 15:29:19 +0200

    feat/pollub-2

---
+++ src/main/java/org/example/user.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class user {
+    public void addUser(String name) {
+        System.out.println("noelo");
+    }
+
+    public void deleteUser(String name) {
+        System.out.println("deleted");
+    }
+}
